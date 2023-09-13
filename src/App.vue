408bed4cc3e18eff5bed285cd040ef4e29397b4d
santi <tiagohig70@gmail.com>
2023-09-12 21:25:30 -0500

    Navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderNav/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/HeaderNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderNav
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #fefcfb;
+}
+
+h1, h2, h3, h4, h5, h6, .centered-title {
+    font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+    font-weight: bold;
+    color: #222;
+}
+
+.centered-title {
+    font-size: 6vh;
+    text-align: center;
+}
+
+.resaltar {
+    color: #fa7436;
+}
+
+p {
+    color: #666;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.prf {
+    text-align: center;
+    font-size: 2.5vh;
+}
+
+footer h5,
+.card-title {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: bold;
+}
+
+#to-do,
+#vacation-plan {
+    padding: 15vh 0;
+}
+
+.fila {
+    padding: 2.5rem 0 5rem;
+}
+
+.flechas button {
+    background: white;
+    border: 1px solid #666;
+    border-radius: 50%;
+}
+
+.flechas button i {
+    color: #666;
+    font-size: 1.5rem;
+}
+
+.flechas button:hover {
+    background: #fa7436;
+    border: none;
+}
+
+.flechas button:hover i {
+    color: white;
+}
+
+.flechas button:active {
+    background-color: white;
+    border: none;
+}
+
+.flechas button:active i {
+    color: #fa7436;
 }
 </style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,82 @@
+<template>
+    <nav class="navbar navbar-expand-lg" aria-label="Eighth navbar example">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img
+                    src="../assets/img/Logo.png"
+                    alt="Logo de Trabook"
+                />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="myNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#destination"
+                            >Destination</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#tour">Tour</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#blog">Blog</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="justify-content-end align-items-center">
+                <button type="button" id="login" class="btn">Login</button>
+                <button type="button" id="sign-up" class="btn">Sign Up</button>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.navbar-collapse {
+    align-items: center;
+    justify-content: center;
+}
+
+#login,
+#sign-up {
+    font-weight: 600;
+    padding: 1vh 2vh;
+    width: 7rem;
+    margin: .5rem 1rem;
+}
+
+nav .nav-item {
+    padding: 0.1em 1em;
+}
+
+#login {
+    color: #fa7436;
+    border: 1px solid #fa7436;
+}
+
+#login:hover {
+    background-color: #fa7436;
+    color: white;
+}
+
+#sign-up {
+    background-color: #fa7436;
+    color: white;
+}
+
+#sign-up:hover {
+    background-color: white;
+    color: #fa7436;
+    border: 1px solid #fa7436;
+}
+</style>
